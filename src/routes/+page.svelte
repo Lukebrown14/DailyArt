acbc5facf3587c697372f99ecd133aaeafc04793
Luke <luke@Lukes-MacBook-Air.local>
2024-06-06 13:12:18 +0100

    Added Basic API Request

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte';
+    let artTile;
+    let artist;
+    let imgId;
+    let description;
+    
+    onMount(async () => {
+      const response = await fetch('https://api.artic.edu/api/v1/artworks')
+      const apiData = await response.json();
+      artTile = apiData.data[0].title;
+      artist = apiData.data[0].artist_title;
+      imgId = apiData.data[0].image_id;
+      description = apiData.data[0].description;
+      console.log(apiData.data[0])
+    });
+</script>
+
+<div>
+    {#if artTile} 
+      <p>Title: {artTile}</p>
+      <p>Artist: {artist}</p>
+
+      <a data-sveltekit-preload-data="tap" href="https://www.artic.edu/iiif/2/{imgId}/full/843,/0/default.jpg">
+        See Art Work
+      </a>
+
+    <p>{description} </p>
+
+    {:else}
+        <p>loading.....</p>
+    {/if}
+</div>
+
