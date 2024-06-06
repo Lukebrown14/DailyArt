170ad7fe9201869a454c12313f51c61014b3f716
Luke <luke@Lukes-MacBook-Air.local>
2024-06-06 13:12:25 +0100

    Added Basic API Request

---
+++ src/routes/store.js
@@ -0,0 +1,21 @@
+import { writable, derived } from 'svelte/store';
+
+/** Store for your data. 
+This assumes the data you're pulling back will be an array.
+If it's going to be an object, default this to an empty object.
+**/
+export const apiData = writable([]);
+console.log(apiData)
+/** 
+ * Data transformation.
+For our use case, we only care about the drink names, not the other information.
+Here, we'll create a derived store to hold the drink names.
+**/
+export const title = derived(apiData, ($apiData) => {
+  
+    console.log('-->', $apiData)
+  if ($apiData.data[0].title){
+    return $apiData.data[0].title;
+  }
+  return [];
+});
